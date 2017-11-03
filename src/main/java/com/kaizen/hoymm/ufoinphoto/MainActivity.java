a31e172ed344d2bc71b59b78400be0a0652c518a
Damian <hoymm241@wp.pl>
2017-11-03 14:48:22 +0100

    Add basics features of main menu

--- src/main/java/com/kaizen/hoymm/ufoinphoto/MainActivity.java
+++ src/main/java/com/kaizen/hoymm/ufoinphoto/MainActivity.java
@@ -2,12 +2,35 @@ package com.kaizen.hoymm.ufoinphoto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button galleryButton, captureUsingCameraButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initButtons();
+        setButtonsActions();
+    }
+
+    private void initButtons() {
+        galleryButton = (Button) findViewById(R.id.galleryButtonId);
+        captureUsingCameraButton = (Button) findViewById(R.id.captureUsingCameraButtonId);
+    }
+
+    private void setButtonsActions() {
+        galleryButton.setOnClickListener(getGalleryButtonAction());
+        captureUsingCameraButton.setOnClickListener(getCaptureUsingCameraButtonAction());
+    }
+
+    private View.OnClickListener getGalleryButtonAction() {
+        return null;
+    }
+
+    private View.OnClickListener getCaptureUsingCameraButtonAction() {
+        return null;
     }
 }
